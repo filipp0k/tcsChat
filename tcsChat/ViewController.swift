cc9d2a67de67839788189bafea1a4be81f719b84
Филипп Дюдин <Filipp0k@MacBook-Pro-Filipp.local>
2017-03-05 19:12:21 +0300

    Initial Commit

---
+++ tcsChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tcsChat
+//
+//  Created by Филипп Дюдин on 05.03.17.
+//  Copyright © 2017 Филипп Дюдин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
